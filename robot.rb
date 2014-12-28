3461495844587efb713c4e4bcd4e09fad033d0e7
Charlotte Chang <yellowspiderlove@gmail.com>
2014-12-28 18:39:50 -0500

    Remove extra spaces

--- robot.rb
+++ robot.rb
@@ -7,10 +7,10 @@ class Robot
 
 	def reset
 		letters = (('a'..'z').to_a + ('A'..'Z').to_a)
-		letters_name = "#{letters[rand(52)]}""#{letters[rand(52)]}"
+		letters_name = "#{letters[rand(52)]}#{letters[rand(52)]}"
 
 		numbers = 3.times.map{ Random.new.rand(0..9)}.join
-		@name = "#{letters_name}""#{numbers}"
+		@name = "#{letters_name}#{numbers}"
 	end
 
 end
