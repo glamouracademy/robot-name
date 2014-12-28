c8c945bc69e42e5b99c58e5cd5233bc4fd22ea75
Charlotte Chang <charlottechang@Charlottes-MacBook-Pro.local>
2014-12-28 17:32:40 -0500

    Random name generates

--- robot.rb
+++ robot.rb
@@ -7,7 +7,7 @@ class Robot
 		letters = (('a'..'z').to_a + ('A'..'Z').to_a).inject(:+).split("")
 		letters_name = "#{letters[rand(52)]}"+"#{letters[rand(52)]}"
 
-		numbers = 3.times.map{ Random.new.rand(0..9)}
-		
+		numbers = 3.times.map{ Random.new.rand(0..9)}.join
+		name = letters_name + numbers
 	end
 end
